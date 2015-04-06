f6888d9622ad617dc249862f645325d86e1e7545
Sergey Kojoian <kojoiansergey@gmail.com>
2015-04-06 10:43:54 -0700

    Planet(.java) class implemented

---
+++ hw1/Planet.java
@@ -0,0 +1,23 @@
+/** Planet.java
+*   author: S. K.
+*   
+*   Implementation of the planet type
+*/
+public class Planet {
+	public double x;
+	public double y;
+	public double xVelocity;
+	public double yVelocity;
+	public double mass;
+	public String img;
+
+	public Planet(double x, double y, double xVelocity, double yVelocity, 
+		         double mass, String img){
+		this.x = x;
+		this.y = y;
+		this.xVelocity = xVelocity;
+		this.yVelocity = yVelocity;
+		this.mass = mass;
+		this.img = img;
+	}
+}
