bda6bfe6bf4ea00d739281db8b4b1b391061fddb
Sergey Kojoian <kojoiansergey@gmail.com>
2015-04-16 02:44:21 -0700

    lec8 code implemented

--- lec8/exercises/hardMode/AList.java
+++ lec8/exercises/hardMode/AList.java
@@ -4,30 +4,59 @@
 
 public class AList {
     /** Creates an empty list. */
+    private int size;
+    public int[] items;
+
     public AList() {
+    	items = new int[100];
+    	size = 0;
     }
 
     /** Inserts X into the back of the list. */
     public void insertBack(int x) {
+    	if (size == items.length){
+    		int[] newItems = new int[size*2];
+    		System.arraycopy(items, 0, newItems, 0, size);
+    		newItems[size] = x;
+    		items = newItems;
+    	} else {
+     		items[size] = x;
+    		 		
+    	}
+    	size += 1;  
     }
 
     /** Returns the item from the back of the list. */
     public int getBack() {
-        return 0;        
+    	if (size == 0){
+    		return Integer.MIN_VALUE;
+    	}
+        	return items[size-1];        
+    	
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        if (i < 0 || i >= size){
+        	return Integer.MIN_VALUE;
+        } else {
+        	return items[i];
+        }        
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int deleteBack() {
-        return 0;
+    	if (size == 0){
+    		return Integer.MIN_VALUE;
+    	} else {
+    		int result = items[size-1];
+         		size = size - 1;
+         		return result;
+    	}
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        	return size;        
     }
 } 
--- lec8/exercises/hardMode/AListTest.java
+++ lec8/exercises/hardMode/AListTest.java
@@ -20,19 +20,39 @@ public class AListTest {
         assertEquals(2, L.size());
     }
 
-    /*
+    
     @Test
     public void testInsertAndGet() {
-    }*/
+    	AList L = new AList();
+    	L.insertBack(10);
+    	L.insertBack(20);
+    	assertEquals(20, L.getBack());
+    	assertEquals(20, L.get(1));
+    	L.insertBack(30);
+    	assertEquals(30, L.getBack());
+    	assertEquals(Integer.MIN_VALUE, L.get(3));
+    }
 
     /*
     @Test
     public void testGet() {
     }*/
 
-    /*@Test
+    @Test
     public void testDelete() {
-    }*/
+    	AList L = new AList();
+    	L.insertBack(20);
+    	L.insertBack(10);
+    	assertEquals(2, L.size());
+    	L.deleteBack();
+    	assertEquals(20, L.getBack());
+   	L.deleteBack();
+   	assertEquals(0, L.size());
+
+    	L = new AList();
+    	L.deleteBack();
+    	assertArrayEquals(new int[100], L.items);
+    }
 
     /** Tests insertion of a large number of items.*/
     /*@Test
